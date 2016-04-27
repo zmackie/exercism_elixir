5b30e56794e97c7aadfa851c5e33539af3847292
Zander Mackie <zmackie@gmail.com>
2016-04-27 17:02:42 -0400

    Break wordcount into multiple functions

--- bob/bob.exs
+++ bob/bob.exs
@@ -3,7 +3,7 @@ defmodule Bob do
     cond do
         String.strip(input) |> String.length == 0 -> "Fine. Be that way!"
         String.last(input) == "?" -> "Sure."
-        String.upcase(input) == input && String.downcase(input) != input -> "Whoa, chill out!"
+        String.upcase(input) == input && String.downcase(input) != input != input  -> "Whoa, chill out!"
         true -> "Whatever."
 
     end
--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,6 +6,17 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
-
+    sentence
+      |> String.split |> count_words
   end
+
+  def count_words(words) do
+    Enum.reduce words, %{}, &update_counts/2
+  end
+
+  def update_counts(word, counts) do
+    Map.update(counts, word, 1, &add_one/1)
+  end
+
+  def add_one(x), do: x + 1
 end
